0adf3b9af55806c6fdeb9e83108887fd8304787a
Farah El Alem <Farah.el1996@gmail.com>
2026-09-16 15:53:23 +0100

    Cache sync module in PWA

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,9 +1,10 @@
-const CACHE = "timepilot-v6";
+const CACHE = "timepilot-v7";
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css",
   "./app.js",
+  "./sync-ui.js",
   "./core.js",
   "./db.js",
   "./manifest.webmanifest",
